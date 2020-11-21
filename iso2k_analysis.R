9bc3d5532ef22803afa03589887faa3e1d2cf0a2
Matthew Berens <bere0174@umn.edu>
2020-11-21 06:42:37 -0600

    update to iso2k_analysis

---
+++ iso2k_analysis.R
@@ -0,0 +1,33 @@
+# =============================================================================
+# set display options
+# =============================================================================
+
+options(scipen = 10, digits = 4) # removes scientific notation
+
+# =============================================================================
+# load required packages
+# =============================================================================
+
+library(magrittr)
+library(tidyverse)
+library(lipdR)
+library(devtools)
+library(ggplot2)
+
+# =============================================================================
+# load Iso2k database
+# =============================================================================
+
+load("iso2k1_0_0.RData") # change this to match the current R serialization.
+#rm(D, TS) # Remove extraneous objects
+mapLipd(D, global = TRUE,size = 3) + ggtitle("iso2k v1.0.0") # Visual archive coverage
+
+#==============================================================================
+# Exclude non-isotope data
+#==============================================================================
+
+paleoData_isotopes <- sTS %>% 
+  filterTs("paleoData_variableName == d18O || d2H") %>%
+  filterTs("paleoData_units == permil") %>%
+  filterTs("paleoData_iso2kPrimaryTimeseries == TRUE")
+
